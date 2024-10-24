0d61506ff3967ae420e9ffeabcfbd6652f51fa26
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-10-24 18:59:22 -0400

    update pyproject; minor additions to label.py; notes on labels

--- paleo_utils/label.py
+++ paleo_utils/label.py
@@ -22,8 +22,44 @@ class Label(ABC):
         save_format: str,
         font_path: str,
         font_size: int,
+        label_dimensions: tuple[int],
+        dimension_type: str = "metric",
     ) -> None:
         self.save_path = save_path
         self.save_format = save_format
         self.font_path = font_path
         self.font_size = font_size
+        self.label_dimensions = label_dimensions
+        self.dimension_type = dimension_type
+
+    def load_label_font(self):
+        pass
+
+    def save_label(self):
+        pass
+
+
+class CollectionsLabel(Label):
+
+    def __init__(
+        self,
+        save_path: str,
+        save_format: str,
+        font_path: str,
+        font_size: int,
+        label_dimensions: tuple[int],
+        dimension_type: str,
+        taxon_rank: str,
+        scientific_name: str,
+        author_citation: str,
+        discovery_year: str,
+    ) -> None:
+
+        super().__init__(
+            save_path,
+            save_format,
+            font_path,
+            font_size,
+            label_dimensions,
+            dimension_type,
+        )
