1e28b55ce5d5e6b5d2ea5f781a34510411e31bb3
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2024-11-02 20:49:35 -0400

    further systematics developments

--- paleo_utils/label.py
+++ paleo_utils/label.py
@@ -213,6 +213,11 @@ class Label(ABC):
         Whether to scale the label text to
         accommodate the image or scale the
         label size instead.
+
+    to_hide
+        A list of the attributes of the label
+        to not display the group name for.
+        Defaults to None.
     """
 
     save_directory: str
@@ -293,6 +298,8 @@ class Label(ABC):
         validator=attrs.validators.in_([50, 500]),
     )
     override_size_w_image: bool = True
+
+    to_hide: list[str] | None = None
 
     def save(self):
         """
@@ -539,6 +546,7 @@ class CollectionsLabel(Label):
     size_title: str = "Size: "
     link: str | None = None
     link_title: str = "Link: "
+    to_hide: list[str]
 
     # def format_label_based_on_dimensions(
     #     self,
@@ -577,26 +585,426 @@ class SystematicsLabel(Label):
     A label class for individual or group systematics,
     containing taxonomic details.
 
+    Attributes
+    ----------
+    description:
+        A short description of the specimen
+        and or context surrounding the fossil.
+        Defaults to None.
 
+    description_title
+        The name of the description group
+        on the label. Defaults to
+        "Description: ".
+
+    domain
+        The name of the domain of the specimen.
+        The domain group defaults to "Domain: ".
+
+    domain_author
+        The citation for the domain name.
+        Defaults to "".
+
+    subdomain
+        The name of the subdomain of the specimen.
+        The subdomain group defaults to "Subdomain: ".
+
+    subdomain_author
+        The citation for the subdomain name.
+        Defaults to "".
+
+    kingdom
+        The name of the kingdom of the specimen.
+        The kingdom group defaults to "Kingdom: ".
+
+    kingdom_author
+        The citation for the kingdom name.
+        Defaults to "".
+
+    subkingdom
+        The name of the subkingdom of the specimen.
+        The subkingdom group defaults to "Subkingdom: ".
+
+    subkingdom_author
+        The citation for the subkingdom name.
+        Defaults to "".
+
+    infrakingdom
+        The name of the infrakingdom of the specimen.
+        The infrakingdom group defaults to "Infrakingdom: ".
+
+    infrakingdom_author
+        The citation for the infrakingdom name.
+        Defaults to "".
+
+    superphylum
+        The name of the superphylum of the specimen.
+        The superphylum group defaults to "Superphylum: ".
+
+    superphylum_author
+        The citation for the superphylum name.
+        Defaults to "".
+
+    phylum
+        The name of the phylum (or division in botany) of the specimen.
+        The phylum group defaults to "Phylum: ".
+
+    phylum_author
+        The citation for the phylum name.
+        Defaults to "".
+
+    subphylum
+        The name of the subphylum of the specimen.
+        The subphylum group defaults to "Subphylum: ".
+
+    subphylum_author
+        The citation for the subphylum name.
+        Defaults to "".
+
+    infraphylum
+        The name of the infraphylum of the specimen.
+        The infraphylum group defaults to "Infraphylum: ".
+
+    infraphylum_author
+        The citation for the infraphylum name.
+        Defaults to "".
+
+    microphylum
+        The name of the microphylum of the specimen.
+        The microphylum group defaults to "Microphylum: ".
+
+    microphylum_author
+        The citation for the microphylum name.
+        Defaults to "".
+
+    superclass
+        The name of the superclass of the specimen.
+        The superclass group defaults to "Superclass: ".
+
+    superclass_author
+        The citation for the superclass name.
+        Defaults to "".
+
+    class
+        The name of the class of the specimen.
+        The class group defaults to "Class: ".
+
+    class_author
+        The citation for the class name.
+        Defaults to "".
+
+    subclass
+        The name of the subclass of the specimen.
+        The subclass group defaults to "Subclass: ".
+
+    subclass_author
+        The citation for the subclass name.
+        Defaults to "".
+
+    infraclass
+        The name of the infraclass of the specimen.
+        The infraclass group defaults to "Infraclass: ".
+
+    infraclass_author
+        The citation for the infraclass name.
+        Defaults to "".
+
+    parvclass
+        The name of the parvclass of the specimen.
+        The parvclass group defaults to "Parvclass: ".
+
+    parvclass_author
+        The citation for the parvclass name.
+        Defaults to "".
+
+    superorder
+        The name of the superorder of the specimen.
+        The superorder group defaults to "Superorder: ".
+
+    superorder_author
+        The citation for the superorder name.
+        Defaults to "".
+
+    order
+        The name of the order of the specimen.
+        The order group defaults to "Order: ".
+
+    order_author
+        The citation for the order name.
+        Defaults to "".
+
+    suborder
+        The name of the suborder of the specimen.
+        The suborder group defaults to "Suborder: ".
+
+    suborder_author
+        The citation for the suborder name.
+        Defaults to "".
+
+    infraorder
+        The name of the infraorder of the specimen.
+        The infraorder group defaults to "Infraorder: ".
+
+    infraorder_author
+        The citation for the infraorder name.
+        Defaults to "".
+
+    parvorder
+        The name of the parvorder of the specimen.
+        The parvorder group defaults to "Parvorder: ".
+
+    parvorder_author
+        The citation for the parvorder name.
+        Defaults to "".
+
+    superfamily
+        The name of the superfamily of the specimen.
+        The superfamily group defaults to "Superfamily: ".
+
+    superfamily_author
+        The citation for the superfamily name.
+        Defaults to "".
+
+    family
+        The name of the family of the specimen.
+        The family group defaults to "Family: ".
+
+    family_author
+        The citation for the family name.
+        Defaults to "".
+
+    subfamily
+        The name of the subfamily of the specimen.
+        The subfamily group defaults to "Subfamily: ".
+
+    subfamily_author
+        The citation for the subfamily name.
+        Defaults to "".
+
+    infrafamily
+        The name of the infrafamily of the specimen.
+        The infrafamily group defaults to "Infrafamily: ".
+
+    infrafamily_author
+        The citation for the infrafamily name.
+        Defaults to "".
+
+    supertribe
+        The name of the supertribe of the specimen.
+        The supertribe group defaults to "Supertribe: ".
+
+    supertribe_author
+        The citation for the supertribe name.
+        Defaults to "".
+
+    tribe
+        The name of the tribe of the specimen.
+        The tribe group defaults to "Tribe: ".
+
+    tribe_author
+        The citation for the tribe name.
+        Defaults to "".
+
+    subtribe
+        The name of the subtribe of the specimen.
+        The subtribe group defaults to "Subtribe: ".
+
+    subtribe_author
+        The citation for the subtribe name.
+        Defaults to "".
+
+    genus
+        The name of the genus of the specimen.
+        The genus group defaults to "Genus: ".
+
+    genus_author
+        The citation for the genus name.
+        Defaults to "".
+
+    subgenus
+        The name of the subgenus of the specimen.
+        The subgenus group defaults to "Subgenus: ".
+
+    subgenus_author
+        The citation for the subgenus name.
+        Defaults to "".
+
+    section
+        The name of the section within the genus.
+        The section group defaults to "Section: ".
+
+    section_author
+        The citation for the section name.
+        Defaults to "".
+
+    subsection
+        The name of the subsection within the genus.
+        The subsection group defaults to "Subsection: ".
+
+    subsection_author
+        The citation for the subsection name.
+        Defaults to "".
+
+    series
+        The name of the series within the genus.
+        The series group defaults to "Series: ".
+
+    series_author
+        The citation for the series name.
+        Defaults to "".
+
+    subseries
+        The name of the subseries within the genus.
+        The subseries group defaults to "Subseries: ".
+
+    subseries_author
+        The citation for the subseries name.
+        Defaults to "".
+
+    species
+        The scientific name of the species.
+        The species group defaults to "Species: ".
+
+    species_author
+        The citation for the species name.
+        Defaults to "".
+
+    subspecies
+        The name of the subspecies of the specimen.
+        The subspecies group defaults to "Subspecies: ".
+
+    subspecies_author
+        The citation for the subspecies name.
+        Defaults to "".
+
+    variety
+        The name of the variety of the specimen.
+        The variety group defaults to "Variety: ".
+
+    variety_author
+        The citation for the variety name.
+        Defaults to "".
+
+    subvariety
+        The name of the subvariety of the specimen.
+        The subvariety group defaults to "Subvariety: ".
+
+    subvariety_author
+        The citation for the subvariety name.
+        Defaults to "".
+
+    form
+        The name of the form of the specimen.
+        The form group defaults to "Form: ".
+
+    form_author
+        The citation for the form name.
+        Defaults to "".
+
+    subform
+        The name of the subform of the specimen.
+        The subform group defaults to "Subform: ".
+
+    subform_author
+        The citation for the subform name.
+        Defaults to "".
     """
 
-    general_description: str
-    domain_name: str
-    domain_author: str
-    kingdom_name: str
-    kingdom_author: str
-    phylum_name: str
-    phylum_author: str
-    class_name: str
-    class_author: str
-    order_name: str
-    order_author: str
-    family_name: str
-    family_author: str
-    genus_name: str
-    genus_author: str
-    species_name: str
-    specimen_author: str
+    # domain Level
+    domain: str = "Domain: "
+    domain_author: str = ""
+    subdomain: str = "Subdomain: "
+    subdomain_author: str = ""
+
+    # kingdom Level
+    kingdom: str = "Kingdom: "
+    kingdom_author: str = ""
+    subkingdom: str = "Subkingdom: "
+    subkingdom_author: str = ""
+    infrakingdom: str = "Infrakingdom: "
+    infrakingdom_author: str = ""
+    superphylum: str = "Superphylum: "
+    superphylum_author: str = ""
+
+    # phylum (or division) Level
+    phylum: str = "Phylum: "
+    phylum_author: str = ""
+    subphylum: str = "Subphylum: "
+    subphylum_author: str = ""
+    infraphylum: str = "Infraphylum: "
+    infraphylum_author: str = ""
+    microphylum: str = "Microphylum: "
+    microphylum_author: str = ""
+    superclass: str = "Superclass: "
+    superclass_author: str = ""
+
+    # class Level
+    class_level: str = "Class: "
+    class_author: str = ""
+    subclass: str = "Subclass: "
+    subclass_author: str = ""
+    infraclass: str = "Infraclass: "
+    infraclass_author: str = ""
+    parvclass: str = "Parvclass: "
+    parvclass_author: str = ""
+    superorder: str = "Superorder: "
+    superorder_author: str = ""
+
+    # order Level
+    order: str = "Order: "
+    order_author: str = ""
+    suborder: str = "Suborder: "
+    suborder_author: str = ""
+    infraorder: str = "Infraorder: "
+    infraorder_author: str = ""
+    parvorder: str = "Parvorder: "
+    parvorder_author: str = ""
+    superfamily: str = "Superfamily: "
+    superfamily_author: str = ""
+
+    # family Level
+    family: str = "Family: "
+    family_author: str = ""
+    subfamily: str = "Subfamily: "
+    subfamily_author: str = ""
+    infrafamily: str = "Infrafamily: "
+    infrafamily_author: str = ""
+    supertribe: str = "Supertribe: "
+    supertribe_author: str = ""
+
+    # tribe Level
+    tribe: str = "Tribe: "
+    tribe_author: str = ""
+    subtribe: str = "Subtribe: "
+    subtribe_author: str = ""
+
+    # genus Level
+    genus: str = "Genus: "
+    genus_author: str = ""
+    subgenus: str = "Subgenus: "
+    subgenus_author: str = ""
+    section: str = "Section: "
+    section_author: str = ""
+    subsection: str = "Subsection: "
+    subsection_author: str = ""
+    series: str = "Series: "
+    series_author: str = ""
+    subseries: str = "Subseries: "
+    subseries_author: str = ""
+
+    # species Level
+    species: str = "Species: "
+    species_author: str = ""
+    subspecies: str = "Subspecies: "
+    subspecies_author: str = ""
+    variety: str = "Variety: "
+    variety_author: str = ""
+    subvariety: str = "Subvariety: "
+    subvariety_author: str = ""
+    form: str = "Form: "
+    form_author: str = ""
+    subform: str = "Subform: "
+    subform_author: str = ""
 
     def format_label_based_on_dimensions(
         self,
