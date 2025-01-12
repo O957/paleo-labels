cb3bab7cc35068f07123b9df29f82f341fecb2c2
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-01-12 01:18:14 -0500

    further mpl label making

--- notebooks/mpl_text_plots.py
+++ notebooks/mpl_text_plots.py
@@ -85,3 +85,59 @@ for i, t in enumerate(texts):
 # save or display the figure
 plt.savefig("label_with_text_boxes.png", dpi=300)
 plt.show()
+
+# %%
+
+fig, ax = plt.subplots(figsize=label_dimensions)
+ax.set_xlim(0, label_dimensions[0])
+ax.set_ylim(0, label_dimensions[1])
+ax.set_aspect("equal")
+ax.axis("off")
+ax.add_patch(Rectangle((0, 0), *label_dimensions, color="lightyellow"))
+border_size = 0.1  # in inches
+border_padding = 0.2  # in inches
+left = border_padding
+bottom = border_padding
+right = label_dimensions[0] - border_padding
+top = label_dimensions[1] - border_padding
+ax.add_patch(
+    Rectangle(
+        (left, bottom),
+        right - left,
+        top - bottom,
+        edgecolor="black",
+        facecolor="none",
+        linewidth=border_size * 12,
+    )
+)
+
+text_content = f"""\
+ID Number:{" "*20}3244
+Collection: AMNH
+Collector: Dr. Montague
+Location: North America
+Formation: Navesink
+Coordinates: (40.7128, -74.0060)
+Date Found: 2024-01-01"""
+
+font_path = fm.findfont(fm.FontProperties(family="Arial"))
+font = fm.FontProperties(fname=font_path, size=14)
+
+ax.text(
+    label_dimensions[0] / 2,
+    label_dimensions[1] / 2,
+    text_content,
+    ha="center",
+    va="center",
+    fontproperties=font,
+    bbox=dict(boxstyle="round,pad=0.5", facecolor="white", edgecolor="black"),
+)
+
+
+output_file_svg = "aligned_text_box.svg"
+plt.savefig(output_file_svg, format="svg")
+print(f"Saved as SVG: {output_file_svg}")
+
+plt.savefig("centered_text_box.png", dpi=300)
+plt.show()
+# %%
