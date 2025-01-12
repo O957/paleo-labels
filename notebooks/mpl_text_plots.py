e748350e47132a9c54372a814ea40db2431bc939
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-01-12 01:05:05 -0500

    mpl label testing

---
+++ notebooks/mpl_text_plots.py
@@ -0,0 +1,87 @@
+# %%
+
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+# %%
+
+# label dimensions in inches
+label_dimensions = (6, 4)  # width, height
+
+# create the label body
+fig, ax = plt.subplots(figsize=label_dimensions)
+ax.set_xlim(0, label_dimensions[0])
+ax.set_ylim(0, label_dimensions[1])
+ax.set_aspect("equal")
+ax.axis("off")
+
+# add background color
+ax.add_patch(Rectangle((0, 0), *label_dimensions, color="lightblue"))
+
+# add border
+border_size = 0.1  # in inches
+border_padding = 0.2  # in inches
+left = border_padding
+bottom = border_padding
+right = label_dimensions[0] - border_padding
+top = label_dimensions[1] - border_padding
+
+ax.add_patch(
+    Rectangle(
+        (left, bottom),
+        right - left,
+        top - bottom,
+        edgecolor="black",
+        facecolor="none",
+        linewidth=border_size * 72,
+    )
+)  # Convert inches to points
+
+# add text boxes
+texts = [
+    {"text": "First Text Box", "font": "Arial", "size": 16, "style": "normal"},
+    {
+        "text": "Second Text Box",
+        "font": "Times New Roman",
+        "size": 14,
+        "style": "italic",
+    },
+    {
+        "text": "Third Text Box (SMALL CAPS)",
+        "font": "Courier New",
+        "size": 12,
+        "style": "small-caps",
+    },
+]
+
+# calculate vertical positions for the text boxes
+box_height = (top - bottom) / len(texts)
+for i, t in enumerate(texts):
+    text_x = label_dimensions[0] / 2  # centered horizontally
+    text_y = top - (i + 0.5) * box_height  # stagger vertically
+
+    # adjust small caps style
+    if t["style"] == "small-caps":
+        t["text"] = t["text"].upper()
+
+    # load font properties
+    font_path = fm.findfont(fm.FontProperties(family=t["font"]))
+    font = fm.FontProperties(fname=font_path, size=t["size"])
+
+    # add text box
+    ax.text(
+        text_x,
+        text_y,
+        t["text"],
+        ha="center",
+        va="center",
+        fontproperties=font,
+        bbox=dict(
+            boxstyle="round,pad=0.5", facecolor="white", edgecolor="black"
+        ),
+    )
+
+# save or display the figure
+plt.savefig("label_with_text_boxes.png", dpi=300)
+plt.show()
