760005185d2754e1df1c28f665ad68c7b0f74c36
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-06-13 02:20:59 -0400

    minor app updates

--- paleo_labels/app.py
+++ paleo_labels/app.py
@@ -4,3 +4,22 @@
 # and loading a style template, or (3) loading a label and
 # a style template a label. Multiple labels can be created
 # in a single session.
+
+import tkinter as tk
+
+
+class PaleoLabelApp:
+    def __init__(self, master):
+        master.title("Paleo-Labels")
+
+        # load toml style or label template file button
+        self.btn_load_style = tk.Button(
+            master, text="Upload Style TOML", command=self.load_style_file
+        )
+        self.btn_load_style.pack(fill="x", padx=10, pady=(10, 5))
+        self.btn_load_label_template = tk.Button(
+            master,
+            text="Upload Label Template TOML",
+            command=self.load_label_template_file,
+        )
+        self.btn_load_label_template.pack(fill="x", padx=10, pady=(10, 5))
