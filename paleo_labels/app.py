dd267b2de0815779afa875b5bdd96830d584e2d6
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-06-13 00:05:24 -0400

    start app; add simple templates

---
+++ paleo_labels/app.py
@@ -0,0 +1,6 @@
+# This script contains the label-making app. The workflow
+# consists of the user either (1) manually entering label
+# and style content, (2) manually entering label content
+# and loading a style template, or (3) loading a label and
+# a style template a label. Multiple labels can be created
+# in a single session.
